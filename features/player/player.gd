938caeecdf1e1ed7b3a9203d8b8bb93a49f92d96
AlixBarreaux <alixbarreaux@tutanota.com>
2026-05-05 16:13:58 +0200

    feat: Add player 8 directional movement

---
+++ features/player/player.gd
@@ -0,0 +1,17 @@
+class_name Player
+extends CharacterBody2D
+
+var speed: float = 200.0
+
+
+func _physics_process(_delta: float) -> void:
+	if not is_multiplayer_authority():
+		return
+	velocity = get_movement_vector() * speed
+	move_and_slide()
+
+
+func get_movement_vector() -> Vector2:
+	if GameState.mode != GameState.Mode.GAMEPLAY:
+		return Vector2.ZERO
+	return Input.get_vector("move_left", "move_right", "move_up", "move_down")
---
+++ systems/game-state/game_state.gd
@@ -0,0 +1,6 @@
+#class_name GameState
+extends Node
+
+enum Mode { GAMEPLAY, UI, PAUSED }
+
+var mode: Mode = Mode.GAMEPLAY
