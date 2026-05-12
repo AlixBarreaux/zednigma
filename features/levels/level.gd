5f3c521b7b68e0874a6f04b13d8d6a2687c78f40
AlixBarreaux <alixbarreaux@tutanota.com>
2026-05-12 22:28:15 +0200

    fix: Only one zombie targets the player on pathfinding
    - Level now assigns the player as a target to ALL zombies

--- features/levels/level.gd
+++ features/levels/level.gd
@@ -2,8 +2,9 @@ class_name Level
 extends Node
 
 @onready var _player: Player = %Player
-@onready var _zombie: Zombie = %Zombie
+@onready var _zombies: Node2D = %Zombies
 
 
 func _ready() -> void:
-	_zombie.set_player_target(_player)
+	for zombie in _zombies.get_children():
+		zombie.set_player_target(_player)
